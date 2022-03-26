273b1e37e45595845a0f678448af68a47e2d56d2
yahfiilham <yahfi.ilham@gmail.com>
2022-03-27 02:38:52 +0700

    add packages for client setup src client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
+import './App.css';
+import Home from './pages/home/Home';
+import Login from './pages/auth/login/Login';
+import Signup from './pages/auth/signup/Signup';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
